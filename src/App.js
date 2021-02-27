a6a59d6e3a361a87513648771e1b93f0b5766f8d
Saqibcode <saqib.u@hotmail.com>
2021-02-27 20:49:25 +0000

    Weather app updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import react, { useState } from "react";
+import moment from "moment"
 
 import "./App.css"
 
@@ -16,7 +17,8 @@ function App() {
 
   const search = evt => {
     if (evt.key === "Enter"){
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      //fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      fetch(`${api.base}forecast?q=${query}&units=metric&APPID=${api.key}`)
       .then(res => res.json())
       .then(result => {
         setWeather(result);
@@ -39,8 +41,18 @@ function App() {
     return `${day} ${date} ${month} ${year}`
     }
 
+    const iconFinder = () =>{
+      //{weather.list[0].weather[0].description}
+      let iconString =""
+      let icon = weather.list[0].weather[0].icon
+      iconString = "http://openweathermap.org/img/wn/"+icon+".png"
+      console.log(iconString)
+      return iconString;
+
+      }
+
   return (
-    <div className={(typeof weather.main != "undefined")? ((weather.main.temp>16)? 'app warm' : 'app'):'app'}>
+    <div className={(typeof weather.city != "undefined")? ((weather.list[0].main.temp>16)? 'app warm' : 'app'):'app'}>
       <main>
         <div className="search-box">
           <input
@@ -52,21 +64,72 @@ function App() {
           onKeyPress={search}
           />
         </div>
-        {(typeof weather.main != "undefined") ? (
+        {(typeof weather.city != "undefined") ? (
           <div>
             <div className ="location-box">
           <div className="location">
-            {weather.name},{weather.sys.country}
+            {weather.city.name},{weather.city.country}
+            
           </div>
           <div className="date">
             {dateBuilder(new Date())}
             </div>
-
           <div className="weather-box">
             <div className="temp">
-              {Math.round(weather.main.temp)}°c
+              {Math.round(weather.list[0].main.temp)}°c
+              <div className="weather">
+              {weather.list[0].weather[0].main}
+              {<img className="icon" src={iconFinder()} alt="Icon"></img>}
+              </div>
+              <div className="weather-description">
+              {weather.list[0].weather[0].description}
+        
+              <br/>
+              <div className="speed">
+              Wind:{(weather.list[0].wind.speed)}mph
+              
             </div>
-            <div className="weather">{weather.weather[0].main}</div>
+            <div className="humidity">
+              Humidity:{(weather.list[0].main.humidity)}%
+            </div>
+              </div>
+
+              
+            </div>
+            
+          </div>
+         
+         
+          <div className="weather-box">
+            <div className="forecast-temp">
+              <>
+                
+                  {(() =>{
+                    let posts = [];
+                    let j=1;
+                    let days = [];
+                    let daysRequired = 4
+                    // days.push
+                    for (let i = 0; i <= daysRequired; i++) {
+                      days.push( moment().add(i, 'days').format('dddd') )
+                    }
+                    
+                    
+                    for(let i=8; i<weather.list.length; i+=8){
+                      
+                        
+                      posts.push(<h4>{days[j]}:{Math.round(weather.list[i].main.temp)}°c</h4>)
+                      
+                      j = j+1
+                    }
+                    return posts
+                  })()}
+                
+              </>
+              
+            </div>
+              
+                
           </div>
 
         </div>
